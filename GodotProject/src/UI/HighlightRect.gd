a6f0291a2277b62e2941a7c84549559da9e7f651
2shady4u <jd16581556@gmail.com>
2020-06-11 14:53:22 +0200

    Cleaned up Tilemap

--- GodotProject/src/UI/HighlightRect.gd
+++ GodotProject/src/UI/HighlightRect.gd
@@ -1,10 +1,13 @@
 extends TextureRect
 
+onready var _area_2D := $Area2D
+
 func _ready():
-	var _error : int = connect("mouse_entered", self, "_on_mouse_entered")
-	_error = connect("mouse_exited", self, "_on_mouse_exited")
+	var _error : int = _area_2D.connect("mouse_entered", self, "_on_mouse_entered")
+	_error = _area_2D.connect("mouse_exited", self, "_on_mouse_exited")
 
 func _on_mouse_entered():
+	print("test")
 	modulate.a = 1
 
 func _on_mouse_exited():
