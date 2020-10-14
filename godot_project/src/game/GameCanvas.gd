fb901b1a2ee55a524cc740d74e1fb69e8cbd40d4
2shady4u <jd16581556@gmail.com>
2020-10-14 17:50:03 +0200

    Remove tiled importer and remake tilemap in Godot

--- godot_project/src/game/GameCanvas.gd
+++ godot_project/src/game/GameCanvas.gd
@@ -2,10 +2,9 @@ extends Node2D
 
 onready var _navigation_2D := $Navigation2D
 onready var _player := $YSort/Player
-onready var _props := $YSort/Props
 onready var _fences := $YSort/Fences
 
-onready var _smog_sprite := $Map/SmogVortex
+onready var _smog_sprite := $SmogVortex
 
 onready var _copper_blockade_shape := $CopperBlockade/CollisionShape2D
 
@@ -19,30 +18,6 @@ func _ready():
 
 #	for ghost in _ghosts.get_children():
 #		_error = ghost.connect("nav_path_requested", self, "_on_nav_path_requested", [ghost])
-
-	return
-	var navigation_polygon_instance := $Navigation2D/NavigationPolygonInstance
-	var navigation_polygon = navigation_polygon_instance.navpoly
-
-	for prop in _props.get_children():
-		#var nav_polygon = NavigationPolygon.new()
-		var outline : PoolVector2Array = prop.get_node("CollisionPolygon2D").polygon
-		print(outline)
-		for i in range(0, outline.size()):
-			print(outline[i].length())
-			outline[i] = (outline[i].length())*outline[i].normalized()
-			outline[i] += prop.position
-#		var rect = Rect2(prop.position - shape.extents, 2*shape.extents)
-#		rect = rect.grow(10)
-#		print(polygon_from_box(rect))
-		#var outline = PoolVector2Array([Vector2(0, 0), Vector2(0, 50), Vector2(50, 50), Vector2(50, 0)])
-		outline.invert()
-		navigation_polygon.add_outline(outline)
-		navigation_polygon.make_polygons_from_outlines()
-		#_navigation_2D.navpoly_add(nav_polygon, Transform2D.IDENTITY)
-
-	navigation_polygon_instance.enabled = false
-	navigation_polygon_instance.enabled = true
 
 func _on_player_nav_path_requested():
 	var mouse_position := get_local_mouse_position()
